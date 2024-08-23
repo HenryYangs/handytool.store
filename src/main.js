5a67c7f088cba90fdb8de6ec1250759ab0c298a9
SIYUAN YANG <sephenry@hotmail.com>
2024-08-23 18:18:29 +0800

    :tada: 项目初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'reset-css';
+import App from './App.svelte'
+
+// Import our custom CSS
+import './style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+})
